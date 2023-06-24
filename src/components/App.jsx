de3a7bb20b152ce731ca4d09d5dad0e6b30848bd
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-06-24 20:41:28 +0300

    hw2phone

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid/non-secure';
+import { ContactsForm } from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onAddPhoneBook = event => {
+    console.log(event);
+    const contact = {
+      ...event,
+      id: nanoid(),
+    };
+    this.setState({
+      contacts: [contact, ...this.state.contacts],
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactsForm onAddPhoneBook={this.onAddPhoneBook} />
+        <ul>
+          {this.state.contacts.map(({ name, number, id }) => (
+            <li key={id}>
+              <p>{name}</p>
+              <p>{number}</p>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// }
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export class ContactsForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  inputChange = e => {
+    // console.log(e.target.name);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  formSubmit = e => {
+    e.preventDefault();
+    const phoneBook = {
+      ...this.state,
+      number: Number(this.state.number),
+    };
+    this.props.onAddPhoneBook(phoneBook);
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.formSubmit}>
+          <label>
+            <span>Name</span>
+            <input
+              onChange={this.inputChange}
+              value={this.state.name}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            <span>Phone</span>
+            <input
+              onChange={this.inputChange}
+              value={this.state.number}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button>Add contact</button>
+        </form>
+      </>
+    );
+  }
+}
